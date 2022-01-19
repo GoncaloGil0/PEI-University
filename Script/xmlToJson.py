6b50512a38f3e8cde7e8bca4bf77bc1253ce4d91
GoncaloGil0 <goncalopedrogil@gmail.com>
2022-01-19 15:06:34 +0000

    implementação da funcionalidade de envio automatico dos dados para o atlas a partir da biblioteca sendAtlas

--- Script/xmlToJson.py
+++ Script/xmlToJson.py
@@ -1,4 +1,4 @@
-import xmltodict, json, sys
+import xmltodict, json, sys, sendAtlas
 
 def escreverDoc(caminho, savePath="./data.json"):
     with open(caminho, 'r') as xmlFile:
@@ -10,7 +10,9 @@ def escreverDoc(caminho, savePath="./data.json"):
         json_file.write(json_object)
         json_file.close()
 
-    return f"| XML --> Json \n| Ficheirio guardado com sucesso em: {savePath}"
+    print(f"| XML --> Json \n| Ficheirio guardado com sucesso em: {savePath}")
+
+    sendAtlas.send(savePath)
 
 def help():
     return (f"+ Instruções + \n| {sys.argv[0]} 'docPath' 'savePath' \n|\n| docPath : diretorio do documento xml \n| savePath: diretorio do documento json (valor por defeito {'./data.json'}) ")
@@ -19,9 +21,9 @@ if sys.argv[1] == "--help":
     print(help())
 elif len(sys.argv) == 3:
     docPath, savePath = sys.argv[1], sys.argv[2]
-    print(escreverDoc(docPath, savePath))
+    escreverDoc(docPath, savePath)
 elif len(sys.argv) == 2:
     docPath = sys.argv[1]
-    print(escreverDoc(docPath))
+    escreverDoc(docPath)
 else :
     print(help())
